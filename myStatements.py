49bde264458e45228eba9451c610754ce720dfa5
Dr. Kandiwapa <kandygirlster210@gmail.com>
2026-05-23 23:29:37 +0200

    adding code to Repo

--- myStatements.py
+++ myStatements.py
@@ -7,3 +7,6 @@ for item in my_list:
 ##cool thing about python is that you can throw away the key with  _ if you don't care about it, and just want the value or vice versa
 for _,value in my_dict.items():
     print(f"My favourite thing to drink is: {value}")
+
+my_dict=my_dict
+
