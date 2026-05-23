71890df2b9d1808bc6c62c2ad239806a51fd3fb1
Dr. Kandiwapa <kandygirlster210@gmail.com>
2026-05-23 22:59:15 +0200

    my second commit

---
+++ myStatements.py
@@ -0,0 +1,9 @@
+my_list = ["apple", "banana", "cherry", "date", "elderberry"]
+my_dict = {"drink": "coffee", "milk": "whole"}
+
+for item in my_list:
+    print(f"My favourit thing to eat is: {item}")
+
+##cool thing about python is that you can throw away the key with  _ if you don't care about it, and just want the value or vice versa
+for _,value in my_dict.items():
+    print(f"My favourite thing to drink is: {value}")
